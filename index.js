8c90a98598061d327066bd2c0662095bb7588f6e
rabehrb203 <165825164+rabehrb203@users.noreply.github.com>
2024-04-20 22:00:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-// const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer");
 const express = require("express");
 const app = express();
 
 app.get("/", async (req, res) => {
-  // const browser = await puppeteer.launch({
-    // args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  // });
-  // const page = await browser.newPage();
-  // await page.goto("https://example.com");
-  // const text = await page.$eval("h1", (el) => el.textContent);
-  res.send("dd");
+ const browser = await puppeteer.launch({
+   args: ["--no-sandbox", "--disable-setuid-sandbox"],
+ });
+  const page = await browser.newPage();
+  await page.goto("https://example.com");
+  const text = await page.$eval("h1", (el) => el.textContent);
+  res.send(text);
 
   await browser.close();
 });
