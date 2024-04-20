0eb90f6acda4c1a89bb2482f37f08b9a7cb0abd3
rabehrb203 <165825164+rabehrb203@users.noreply.github.com>
2024-04-20 23:23:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,41 @@ const puppeteer = require("puppeteer");
 const express = require("express");
 const app = express();
 
+app.get("/mangas", async (req, res) => {
+  try {
+    // استدعاء صفحة الويب المطلوبة باستخدام axios
+    const response = await axios.get("https://mangatak.com/manga/?page=2");
+
+    // تحليل الصفحة باستخدام cheerio
+    const $ = cheerio.load(response.data);
+    const dataList = [];
+
+    // استخراج البيانات من الصفحة
+    $(".listupd .bs").each((index, element) => {
+      const title = $(element)
+        .find(".tt")
+        .text()
+        .substring(5)
+        .replace("\t\t\t", "");
+
+      const image = $(element).find(".ts-post-image").attr("src");
+      const link = $(element)
+        .find("a")
+        .attr("href")
+        .substring(27)
+        .replace("/", "");
+      const chapter = $(element).find(".epxs").text();
+
+      dataList.push({ title, image, chapter, link });
+    });
+
+    // إرسال البيانات كاستجابة
+    res.json(dataList);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 // عرض الصور باستخدام المسار /images/link
 app.get("/images/:link", async (req, res) => {
   try {
