e82220e18d4d74b09dc81671e96fec4856bde9c9
rabehrb203 <165825164+rabehrb203@users.noreply.github.com>
2024-04-20 23:02:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const puppeteer = require("puppeteer");
 const express = require("express");
 const app = express();
 
-app.get("/", async (req, res) => {
+// عرض الصور باستخدام المسار /images/link
+app.get("/images/:link", async (req, res) => {
   try {
+    const link = req.params.link;
     const browser = await puppeteer.launch({
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
     const page = await browser.newPage();
     
-    // تحميل الصفحة المطلوبة
-    await page.goto("https://mangatak.com/6392673069-the-lords-coins-arent-decreasing-chapter-112/");
+    // تحميل الصفحة المطلوبة باستخدام المسار المُحدد
+    await page.goto(`https://mangatak.com/${link}`);
     
     // انتظار حتى يتم تحميل الصفحة بشكل كامل
     await page.waitForSelector('#readerarea img');
@@ -18,7 +20,12 @@ app.get("/", async (req, res) => {
     // استخراج عناصر img داخل div readerarea
     const imageUrls = await page.evaluate(() => {
       const images = Array.from(document.querySelectorAll('#readerarea img'));
-      return images.map(img => img.getAttribute('src')).filter(src => !src.includes('i.ibb.co/hfgDCBF/image.webp'));
+      return images.map(img => {
+        const src = img.getAttribute('src');
+        if (!src.includes('i.ibb.co/hfgDCBF/image.webp')) {
+          return src;
+        }
+      }).filter(Boolean);
     });
     
     await browser.close();
